b880273beb4c0137ac420992f18ef8dd0ae4659d
GALANAN <angelgalan82@gmail.com>
2024-09-23 15:04:04 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +6,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function getRamdomElement(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
+  function generateExcuse() {
+    let excuse =
+      getRamdomElement(who) +
+      " " +
+      getRamdomElement(action) +
+      " " +
+      getRamdomElement(what) +
+      " " +
+      getRamdomElement(when) +
+      ".";
+
+    return excuse;
+  }
+
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
